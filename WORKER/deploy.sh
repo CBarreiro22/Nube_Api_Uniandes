24730f62afd796a00e3e5f570391901af326b0bc
Camilo Barreiro <c.barreiroh@uniandes.edu.co>
2023-05-20 10:44:09 -0500

    Update

--- WORKER/deploy.sh
+++ WORKER/deploy.sh
@@ -3,7 +3,13 @@ gcloud builds submit \
 gcloud run deploy prueba \
   --image gcr.io/$GOOGLE_CLOUD_PROJECT/prueba \
   --platform managed \
-  --region northamerica-northeast2 \
+  --region us-central1 \
   --allow-unauthenticated \
   --max-instances=1 \
-  --port=5000
+  --port=5000 \
+  --set-env-vars "DB_HOST=10.188.0.4" \
+  --set-env-vars "DB_USER=admin" \
+  --set-env-vars "DB_PASSWORD=admin" \
+  --set-env-vars"DB_NAME=apisnube" \
+  --set-env-vars "DB_PORT=5432"
+
